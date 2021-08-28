510e5f98808e26523ae8f05d2de3f0cb24289b09
dongyueqian <874974405@qq.com>
2021-08-28 21:19:09 +0800

    线程安全：本次提交的代码有时正常，有时会出现余额为负数的bug

---
+++ 03_lock_concurrent.py
@@ -0,0 +1,21 @@
+import threading
+
+class Account:
+    def __init__(self, balance):
+        self.balance = balance
+
+def draw(account, amount):
+    if account.balance >= amount:
+        print(threading.current_thread().name, "取钱成功----1")
+        account.balance -= amount
+        print(threading.current_thread().name, "余额----2", account.balance)
+
+    else:
+        print(threading.current_thread().name, "取钱失败,余额不足----3")
+
+if __name__ == '__main__':
+    account = Account(10000)
+    cu1 = threading.Thread(target=draw, args=(account, 8000), name="cu1")
+    cu2 = threading.Thread(target=draw, args=(account, 8000), name="cu2")
+    cu1.start()
+    cu2.start()
