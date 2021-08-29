cbfbe23ffb9e45601337b812e950e3751fc869ba
dongyueqian <874974405@qq.com>
2021-08-29 15:17:51 +0800

    加锁，一个线程执行完成后释放锁，另外一个线程才能执行

--- 03_lock_concurrent.py
+++ 03_lock_concurrent.py
@@ -1,19 +1,22 @@
 import threading
 import time
 
+lock = threading.Lock()
+# print(lock)
+
 class Account:
     def __init__(self, balance):
         self.balance = balance
 
 def draw(account, amount):
-    if account.balance >= amount:
-        time.sleep(0.1) # 会引发阻塞，进而切换线程
-        print(threading.current_thread().name, "取钱成功----1")
-        account.balance -= amount
-        print(threading.current_thread().name, "余额----2", account.balance)
-
-    else:
-        print(threading.current_thread().name, "取钱失败,余额不足----3")
+    with lock:
+        if account.balance >= amount:
+            time.sleep(0.1) # 会引发阻塞，进而切换线程
+            print(threading.current_thread().name, "取钱成功----1")
+            account.balance -= amount
+            print(threading.current_thread().name, "余额----2", account.balance)
+        else:
+            print(threading.current_thread().name, "取钱失败,余额不足----3")
 
 if __name__ == '__main__':
     account = Account(10000)
